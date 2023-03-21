bb34b71b9112f1cb929109f4e8f4ca79a6d9d665
bentezzy <bentezzy@gmail.com>
2023-03-21 16:06:16 -0700

    function that calculates the sum of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers
+ * @a: first parameterr
+ * @b: second parameter
+ *
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}

bb9889dae7e5e082212f3c942b780ef51e9d3c41
bentezzy <bentezzy@gmail.com>
2023-03-21 16:51:51 -0700

    function that print n numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - function that print n numbers
+ *
+ * @n: parameter of function
+ *
+ * return: always 0
+ */
+
+void print_to_98(int n)
+{
+	int f;
+
+	if (n >= 0 && n <= 98)
+	{
+		for (f = n; f <= 98; f++)
+		{
+			printf("%d, ", f);
+		}
+		printf("\n");
+	}
+	else if (n < 0)
+	{
+		for (f = n; f <= 98; f++)
+		{
+			printf("%d, ", f);
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (f = n; f >= 98; f--)
+		{
+			printf("%d, ", f);
+		}
+		printf("\n");
+	}
+	else if (n == 98)
+		printf("%d, ", n);
+	printf("\n");
+}

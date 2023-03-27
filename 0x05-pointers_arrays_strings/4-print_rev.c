55c6b5cb993b50692996e9257339117ef15365b2
bentezzy <bentezzy@gmail.com>
2023-03-27 11:44:43 -0700

    print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,11 @@
 void print_rev(char *s)
 {
 	int a;
-	int length = 0;
+	int count = 0;
 
 	for (a = 0; s[a] != '\0'; a++)
-		length++;
-	for (a = length; a >= 0; a--)
+		count++;
+	for (a = count; a >= 0; a--)
 		_putchar(s[a]);
 	_putchar('\n');
 }

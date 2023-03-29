8edb47ddec71f3326a5cd34078222469326df73e
bentezzy <bentezzy@gmail.com>
2023-03-29 12:46:36 -0700

    lower to upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that converts string to uppercase
+ * @h: array
+ *
+ * Return: h
+ */
+
+char *string_toupper(char *h)
+{
+	int t;
+
+	t = 0;
+	while (h[t] != '\0')
+	{
+		if (h[t] >= 'a' && h[t] <= 'z')
+		{
+			h[t] = h[t] - 32;
+			t++;
+		}
+	}
+	return (h);
+}

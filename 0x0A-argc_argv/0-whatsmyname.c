2121641e0f101875460d96a5a5292ce027dba1d1
bentezzy <bentezzy@gmail.com>
2023-04-10 10:54:44 -0700

    whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,9 @@
 /**
  * main - function that prints name of prog
  * @argc: prints number of arguments
- * @argv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: prints array of argumrnts
+ * @argv: prints array of argumrnts
  *
- * Returns: 0
+ * Return: 0
  */
 
 int main(int argc __attribute__((unused)), char *argv[])
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-int _atoi(char *s);
-int main(int argc __attribute__((unused)), char *argv[]);
 
+int _putchar(char c);
+
+int _atoi(char *s);
 
 #endif

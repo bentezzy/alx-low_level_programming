8622396004852116f3e401d3bdd304258a06598b
bentezzy <bentezzy@gmail.com>
2023-07-14 04:42:29 -0700

    A function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: pointer to the file
+ * @text_content: pointer to the string
+ *
+ * Return: 1 for success or -1 for fail
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, b, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	b = write(fd, text_content, len);
+
+	if (fd == -1 || b == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
